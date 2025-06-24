ee41ba26fb88460c2ebc2f5a07e10954e890ba98
Leandro <leandrosvetlich@gmail.com>
2025-06-24 17:34:40 -0300

    added link to home in header brand

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,11 +1,12 @@
 import { Button } from 'primereact/button'
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Header = () => {
   return (
      <div className="bg-blue-600 text-white p-1 pl-2 text-lg flex justify-content-between shadow-2 align-items-center">
-        <Image src="/icons/dux-50.png" alt="Dux Software"  width={35} height={35}/>
+        <Link href={"/"}><Image src="/icons/dux-50.png" alt="Dux Software"  width={35} height={35}/></Link>
         <Button icon="pi pi-cog"  aria-label="Configuración" text className='text-white'/>
     </div>
   )
